0476be7be734632387f537b416615639840f37f1
Hugo Andres Orozco <hugandoro@gmail.com>
2020-09-16 15:23:15 -0500

    Update TareaController.php

--- app/Http/Controllers/TareaController.php
+++ app/Http/Controllers/TareaController.php
@@ -243,8 +243,11 @@ class TareaController extends Controller
         $planDesarrollo = PlanDesarrollo::where('administracion_id', config('app.administracion'))->with('administracion')->get();
 
         //Hace una primer busqueda GENERAL
-        $tarea = Tarea::orderBy('id','desc')->with('accion','accion.planIndicativo','accion.planIndicativo.vigencia','accion.planIndicativo.indicador','accion.planIndicativo.indicador.Nivel4','accion.planIndicativo.indicador.Nivel4.entidadOficina')->paginate(10);
+        $tarea = Tarea::All();
         $totalTareas = count($tarea);
+
+        //Hace una segunda busqueda GENERAL con resultado paginados
+        $tarea = Tarea::orderBy('id','desc')->with('accion','accion.planIndicativo','accion.planIndicativo.vigencia','accion.planIndicativo.indicador','accion.planIndicativo.indicador.Nivel4','accion.planIndicativo.indicador.Nivel4.entidadOficina')->paginate(10);
 
         //Paginacion de resultados conservando el indice (Metodo GET y no POST)
         $pagination = $tarea->appends(request () -> except (['page', '_token'])) -> links ();
