b77f911010c9c23e3b3b7a5ae12645990efaf468
Hugo Andres Orozco <hugandoro@gmail.com>
2020-12-28 12:23:30 -0500

    Update listarplanaccionreporte.blade.php
    Excepcion temporal POT y Socioeconomica

--- resources/views/planaccion/listarplanaccionreporte.blade.php
+++ resources/views/planaccion/listarplanaccionreporte.blade.php
@@ -157,9 +157,16 @@
                                             <!-- Valida si es un usuario (SUPERADMINISTRADOR O ADMINISTRADOR) o si es un usuario (EDITOR asignado a la DEPENDENCIA) responable de esa actividad Nivel 4 -->
                                             @if( (Auth::user()->hasRole('super')) || (Auth::user()->hasRole('editor') && (Auth::user()->oficina_id) == $Nivel4->oficina_id) )
                                               
-                                              <!-- SUSPENSION TEMPORAL REPORTE POR FIN DE VIGENCIA 2020 Boton de reporte deshabilitado -->
+                                              <!-- *** SUSPENSION TEMPORAL REPORTE POR FIN DE VIGENCIA 2020 Boton de reporte deshabilitado *** -->
                                               <!-- <a class="btn btn-success" href="{{ url('tarea/create?idAccion='.$accion->id.'&kpi='.$accion->kpi.'&kpiObjetivo='.$accion->objetivo) }}" ><span class="glyphicon glyphicon-plus"></span>  Reportar</a> -->
-                                            
+                                              <!-- *** FIN SUSPENSION *** -->
+
+                                              <!-- *** EXCEPCION DE REPORTE TEMPORAL - POT (Maria Fernanda Vera) - SOCIOECONOMICA (Ruth Nora Salazar) *** -->
+                                              @if( (Auth::user()->hasRole('super')) || (Auth::user()->hasRole('editor') && (Auth::user()->oficina_id) == $Nivel4->oficina_id) && ( (Auth::user()->id == '13') || (Auth::user()->id == '50') ) )
+                                                <a class="btn btn-success" href="{{ url('tarea/create?idAccion='.$accion->id.'&kpi='.$accion->kpi.'&kpiObjetivo='.$accion->objetivo) }}" ><span class="glyphicon glyphicon-plus"></span>  Reportar</a>
+                                              @endif
+                                              <!-- *** FIN EXCEPCION *** -->
+
                                             @endif
                                             <!-- Fin de la validacion de permisos para reportar -->
 
