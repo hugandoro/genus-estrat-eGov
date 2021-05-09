370749c7d7987642ceffcdde7ae2eb02e46a7688
Hugo Andres Orozco <hugandoro@gmail.com>
2021-05-09 18:57:06 -0500

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
-        $request->user()->authorizeRoles(['super','admin','editor','supervisor','user']); //Roles autorizados para continuar ejecutando este metodo
+        $request->user()->authorizeRoles(['super','admin','editor','supervisor','user','editorMipg','adminMipg']); //Roles autorizados para continuar ejecutando este metodo
 
         return view('home');
     }
